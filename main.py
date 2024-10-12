bd38d8b1628486f1d4d9ce34e6c2ecbbf9cd3e68
Ramón Puñet (zpunram) <ramon.punet@blue-tc.com>
2024-10-12 20:35:38 +0200

    feat: [swapi-usvc] complete main.py

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import requests
-
-SWAPI_URL = "https://swapi.dev/api/people/"
+import logging
 
 app = FastAPI()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 @app.get("/people")
 def get_people():
-    response = requests.get(SWAPI_URL)
-    people = response.json()
+    url = "https://swapi.dev/api/people/"
+    people = []
 
-    return people
+    try:
+        while url:
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+            people.extend(data["results"])
+            url = data["next"]
+
+        logger.info("Data fetched and sorted")
+        return sorted(people, key=lambda item: item["name"])
+    except:
+        logger.error(f"Error fetching data")
+        raise HTTPException(status_code=500, detail="xxx")
