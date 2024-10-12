0b969eaaf1e906a3ec9df813a9fcea7563e2ce3a
Ramón Puñet (zpunram) <ramon.punet@blue-tc.com>
2024-10-12 14:43:26 +0200

    feat

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import requests
+
+SWAPI_URL = "https://swapi.dev/api/"
+
+app = FastAPI()
+@app.get("/people")
+def get_people():
+    response = requests.get(SWAPI_URL)
+    people = response.json()
+
+    return people
