0c593da616127e56bce6bbdbdfb35a63e8d27a4d
Ramón Puñet (zpunram) <ramon.punet@blue-tc.com>
2024-10-13 19:21:06 +0200

    feat: [swapi-usvc] include test duration, error handling and logs

--- test.py
+++ test.py
@@ -1,20 +1,29 @@
 import requests
 import threading
+import time
 
-url = 'http://100.77.107.161:30007/people'
+url = 'http://100.77.107.161:30007/people'   #use minikube ip
 
 def send_request():
     response = requests.get(url)
+    if response.status_code != 200:
+        print("Request failed")
+    else:
+        print(f"Response code: {response.status_code} OK")
 
-def test_traffic_load(concurrent_requests):
+def test_traffic_load(concurrent_requests, duration):
     threads = []
+    initial_time = time.time()
 
-    for _ in range(concurrent_requests):
-        thread = threading.Thread(target=send_request)
-        thread.start()
-        threads.append(thread)
+    while time.time() - initial_time < duration:
+        for _ in range(concurrent_requests):
+            thread = threading.Thread(target=send_request)
+            thread.start()
+            threads.append(thread)
+
+        time.sleep(1)
 
     for thread in threads:
         thread.join()
 
-test_traffic_load(10)
+test_traffic_load(10, 60)
